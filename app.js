d6a2954f21a5c76d08f49475d3aeae1a2b49caec
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-08-10 11:12:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let words = [["cat"]]
 
-let anagram = () => {
-    for (let i = 0; i < words.length; i++) {
-        console.log(words[i])
+const words = ["bat", "tap", "cat", "tab", "pat"];
+
+function detectAnagrams(words) {
+    const anagrams = {};
+    for (const word of words) {
+        const sortedWord = [...word].sort().join('');
+        if (sortedWord in anagrams) {
+            anagrams[sortedWord].push(word);
+        } else {
+            anagrams[sortedWord] = [word];
+        }
     }
+    return Object.values(anagrams);
 }
 
-anagram()
+console.log(detectAnagrams(words));
